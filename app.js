8773d8bb117f1becde2baf8198aac48b352ae45a
azebusmc <65170242+azebusmc@users.noreply.github.com>
2025-11-07 11:28:43 -0500

    Added in bootstrap, variables for data binding, bootstrap table, and basic styling

---
+++ app.js
@@ -0,0 +1,18 @@
+taskArr = [];
+taskObj = {
+  taskName: "",
+  category: "",
+  dealine: "",
+  status: "",
+};
+
+document.getElementById("submitButton").addEventListener("click", submit);
+
+function submit(event) {
+  console.log("submitted");
+  taskNameInput = document.getElementById("taskNameInput").value;
+  categoryInput = document.getElementById("categoryInput").value;
+  deadlineInput = document.getElementById("deadlineInput").value;
+  initialStatusInput = document.getElementById("initialStatusInput").value;
+  tas
+}
